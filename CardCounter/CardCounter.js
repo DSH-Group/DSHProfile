457505f94c9858fe2c950f745f5747eb4db489a8
DmArkanoid <68047962+DmArkanoid@users.noreply.github.com>
2022-08-10 20:22:15 -0400

    Update CardCounter.js

--- CardCounter/CardCounter.js
+++ CardCounter/CardCounter.js
@@ -4,7 +4,7 @@ let holdBet = ""
 
 function cc(card, deckNumber) {
 
-//The math for counting cards
+  //The math for counting cards
  for (let i = 0; i < card.length; i++) {
   if (card[i] >= 2 && card[i] <= 6){
     count = count + 1;
@@ -23,49 +23,48 @@ function cc(card, deckNumber) {
     count = count - 1;
     break;
   }
-  
  }
  
 
 
 
-// Bet type
-if (count >= 4 && count >= 5){
-  holdBet = "Bet Amount: Low"
-}
-if(count >= 6 && count <=8){
-  holdBet = "Bet Amount: Mid"
-}
-if (count >= 9){
-  holdBet = "Bet Amount: High"
-}
-if (count <= 4 && count >= 2){
-  holdBet = "Bet Type: Hold"
-}
-if (count <= 1){
-  holdBet = "Bet Type: Fold or Hold"
-}
-if (count <= 0){
-  holdBet = "Bet Type: Fold"
-}
-if (deckNumber == undefined){
-  holdBet = holdBet
-}
+  // Bet type
+  if (count >= 4 && count >= 5){
+    holdBet = "Bet Amount: Low"
+  }
+  if(count >= 6 && count <=8){
+    holdBet = "Bet Amount: Mid"
+  }
+  if (count >= 9){
+    holdBet = "Bet Amount: High"
+  }
+  if (count <= 4 && count >= 2){
+    holdBet = "Bet Type: Hold"
+  }
+  if (count <= 1){
+    holdBet = "Bet Type: Fold or Hold"
+  }
+  if (count <= 0){
+    holdBet = "Bet Type: Fold"
+  }
+  if (deckNumber == undefined){
+    holdBet = holdBet
+  }
 
 
 
 
-return holdBet + nc(count, deckNumber)
+  return holdBet + nc(count, deckNumber)
 }
 
 // what to do when you have multiple decks in play
 function nc(fCount, deckNum){
   if (deckNum > 1){
     newCount = fCount / deckNum  
-  } if (deckNum == undefined){
+  } 
+  if (deckNum == undefined){
     return "Please specify the number of decks used."
-  }
-  else {newCount = fCount}
+  } else newCount = fCount
   
   return ", Final Count: " + newCount;
 }
