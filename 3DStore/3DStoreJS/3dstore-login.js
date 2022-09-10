fcbd3c25405cfc14274215b05c70eb6a3adaf075
DmArkanoid <68047962+DmArkanoid@users.noreply.github.com>
2022-09-10 02:05:06 -0400

    3dStore Creation
    This is the works of the 3d store and the beginnings of it.

---
+++ 3DStore/3DStoreJS/3dstore-login.js
@@ -0,0 +1,47 @@
+
+//password toggle
+
+function passVisFunc() {
+    var x = document.getElementById("passwordVis");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+};
+
+function errorMessages(formElement, type, message) {
+    const messageElement = formElement.querySelector(".form__input-error");
+
+    messageElement.textContent = message;
+    messageElement.classList.remove(".form--hidden");
+
+};
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    
+    const loginForm = document.querySelector("#login");
+    const creatAccountForm = document.querySelector("#register");
+    const forgetPassForm = document.querySelector("#forgotPass");
+
+
+    loginForm.addEventListener("submit", e => {
+        e.preventDefault();
+
+        //perform ajax login
+        setFormMessage(loginForm, "error", "#loginError")
+    })
+    creatAccountForm.addEventListener("submit", e => {
+        e.preventDefault();
+        
+        //perform ajax register
+
+    })
+    forgetPassForm.addEventListener("submit", e => {
+        e.preventDefault();
+
+        //perform ajax forget Password
+    })
+});
+
